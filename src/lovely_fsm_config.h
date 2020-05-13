d3bc9ce83145751a0af9de1b3abe14623a2dc60a
Lars Heinrichs <lars.heinrichs@web.de>
2020-05-13 19:13:15 +0200

    Use buffer_c library by default

--- src/lovely_fsm_config.h
+++ src/lovely_fsm_config.h
@@ -7,11 +7,21 @@
 #define LFSM_MAX_COUNT          3
 
 // --- buffer functions ---
+#define USE_BUFFER_C_LIB        1
+
+#if (USE_BUFFER_C_LIB)
 #define buffer_handle_type                   buf_buffer_t
 #define add_to_buffer(buffer_handle, value)  buf_add_element(buffer_handle, value)
 #define read_from_buffer(buffer_handle)      buf_read_element(buffer_handle)
 #define buffer_full(buffer_handle)           buf_is_full(buffer_handle)
 #define buffer_empty(buffer_handle)          buf_is_empty(buffer_handle)
-
+#else
+// provide your own buffer implementation!
+#define buffer_handle_type                   
+#define add_to_buffer(buffer_handle, value)  
+#define read_from_buffer(buffer_handle)      
+#define buffer_full(buffer_handle)           
+#define buffer_empty(buffer_handle)          
+#endif // 
 
 #endif // __LOVELY_FSM_CONFIG_H
