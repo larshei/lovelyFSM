f485cc0355941210681748b5df4fefa916735be8
Lars Heinrichs <lars.heinrichs@web.de>
2020-07-27 09:16:09 +0200

    Add u\nit test for second instance with large 10x10 fsm

--- unit_test/test/test_lovely_fsm.c
+++ unit_test/test/test_lovely_fsm.c
@@ -27,6 +27,9 @@ typedef struct my_data_t {
     uint8_t normal_entry_run_count;
     uint8_t normal_run_run_count;
     uint8_t normal_exit_run_count;
+    uint8_t generic_entry_run_count;
+    uint8_t generic_run_run_count;
+    uint8_t generic_exit_run_count;
 } my_data_t;
 my_data_t my_data;
 
@@ -59,11 +62,15 @@ enum events {
     EV_MEASURE
 };
 
+
 enum states {
     ST_NORMAL = 1,
     ST_ALARM,
     ST_WARN = 4
 };
+
+enum events_large_fsm {EV_0, EV_1, EV_2, EV_3, EV_4, EV_5, EV_6, EV_7, EV_8, EV_9};
+enum states_large_fsm {ST_0, ST_1, ST_2, ST_3, ST_4, ST_5, ST_6, ST_7, ST_8, ST_9};
 
 // -- state transitions: condition functions --
 int temperature_okay(lfsm_t context);
@@ -80,6 +87,9 @@ lfsm_return_t normal_entry(lfsm_t context);
 lfsm_return_t normal_run(lfsm_t context);
 lfsm_return_t normal_exit(lfsm_t context);
 
+lfsm_return_t generic_entry(lfsm_t context);
+lfsm_return_t generic_run(lfsm_t context);
+lfsm_return_t generic_exit(lfsm_t context);
 
 // -------------------------------------------------------------------------
 // - transition table
@@ -92,6 +102,103 @@ lfsm_transitions_t transition_table[] = {
     { ST_WARN   , EV_MEASURE      , temperature_okay     , ST_NORMAL },
     { ST_WARN   , EV_MEASURE      , temperature_critical , ST_ALARM  },
 };
+
+lfsm_transitions_t my_transition_table[] = {
+    { ST_0  , EV_0 , lfsm_always , ST_0 },
+    { ST_0  , EV_1 , lfsm_always , ST_1 },
+    { ST_0  , EV_2 , lfsm_always , ST_2 },
+    { ST_0  , EV_3 , lfsm_always , ST_3 },
+    { ST_0  , EV_4 , lfsm_always , ST_4 },
+    { ST_0  , EV_5 , lfsm_always , ST_5 },
+    { ST_0  , EV_6 , lfsm_always , ST_6 },
+    { ST_0  , EV_7 , lfsm_always , ST_7 },
+    { ST_0  , EV_8 , lfsm_always , ST_8 },
+    { ST_0  , EV_9 , lfsm_always , ST_9 },
+    { ST_1  , EV_0 , lfsm_always , ST_0 },
+    { ST_1  , EV_1 , lfsm_always , ST_1 },
+    { ST_1  , EV_2 , lfsm_always , ST_2 },
+    { ST_1  , EV_3 , lfsm_always , ST_3 },
+    { ST_1  , EV_4 , lfsm_always , ST_4 },
+    { ST_1  , EV_5 , lfsm_always , ST_5 },
+    { ST_1  , EV_6 , lfsm_always , ST_6 },
+    { ST_1  , EV_7 , lfsm_always , ST_7 },
+    { ST_1  , EV_8 , lfsm_always , ST_8 },
+    { ST_1  , EV_9 , lfsm_always , ST_9 },
+    { ST_2  , EV_0 , lfsm_always , ST_0 },
+    { ST_2  , EV_1 , lfsm_always , ST_1 },
+    { ST_2  , EV_2 , lfsm_always , ST_2 },
+    { ST_2  , EV_4 , lfsm_always , ST_4 },
+    { ST_2  , EV_5 , lfsm_always , ST_5 },
+    { ST_2  , EV_6 , lfsm_always , ST_6 },
+    { ST_2  , EV_7 , lfsm_always , ST_7 },
+    { ST_2  , EV_8 , lfsm_always , ST_8 },
+    { ST_2  , EV_9 , lfsm_always , ST_9 },
+    { ST_3  , EV_0 , lfsm_always , ST_0 },
+    { ST_3  , EV_1 , lfsm_always , ST_1 },
+    { ST_3  , EV_2 , lfsm_always , ST_2 },
+    { ST_3  , EV_3 , lfsm_always , ST_3 },
+    { ST_3  , EV_4 , lfsm_always , ST_4 },
+    { ST_3  , EV_5 , lfsm_always , ST_5 },
+    { ST_3  , EV_6 , lfsm_always , ST_6 },
+    { ST_3  , EV_7 , lfsm_always , ST_7 },
+    { ST_3  , EV_8 , lfsm_always , ST_8 },
+    { ST_3  , EV_9 , lfsm_always , ST_9 },
+    { ST_4  , EV_1 , lfsm_always , ST_1 },
+    { ST_4  , EV_3 , lfsm_always , ST_3 },
+    { ST_4  , EV_4 , lfsm_always , ST_4 },
+    { ST_4  , EV_5 , lfsm_always , ST_5 },
+    { ST_4  , EV_6 , lfsm_always , ST_6 },
+    { ST_4  , EV_7 , lfsm_always , ST_7 },
+    { ST_4  , EV_9 , lfsm_always , ST_9 },
+    { ST_5  , EV_0 , lfsm_always , ST_0 },
+    { ST_5  , EV_1 , lfsm_always , ST_1 },
+    { ST_5  , EV_2 , lfsm_always , ST_2 },
+    { ST_5  , EV_3 , lfsm_always , ST_3 },
+    { ST_5  , EV_4 , lfsm_always , ST_4 },
+    { ST_5  , EV_5 , lfsm_always , ST_5 },
+    { ST_5  , EV_6 , lfsm_always , ST_6 },
+    { ST_5  , EV_7 , lfsm_always , ST_7 },
+    { ST_5  , EV_8 , lfsm_always , ST_8 },
+    { ST_5  , EV_9 , lfsm_always , ST_9 },
+    { ST_6  , EV_0 , lfsm_always , ST_0 },
+    { ST_6  , EV_1 , lfsm_always , ST_1 },
+    { ST_6  , EV_2 , lfsm_always , ST_2 },
+    { ST_6  , EV_3 , lfsm_always , ST_3 },
+    { ST_6  , EV_5 , lfsm_always , ST_5 },
+    { ST_6  , EV_6 , lfsm_always , ST_6 },
+    { ST_6  , EV_7 , lfsm_always , ST_7 },
+    { ST_6  , EV_8 , lfsm_always , ST_8 },
+    { ST_7  , EV_0 , lfsm_always , ST_0 },
+    { ST_7  , EV_1 , lfsm_always , ST_1 },
+    { ST_7  , EV_2 , lfsm_always , ST_2 },
+    { ST_7  , EV_3 , lfsm_always , ST_3 },
+    { ST_7  , EV_4 , lfsm_always , ST_4 },
+    { ST_7  , EV_6 , lfsm_always , ST_6 },
+    { ST_7  , EV_7 , lfsm_always , ST_7 },
+    { ST_7  , EV_8 , lfsm_always , ST_8 },
+    { ST_7  , EV_9 , lfsm_always , ST_9 },
+    { ST_8  , EV_0 , lfsm_always , ST_0 },
+    { ST_8  , EV_1 , lfsm_always , ST_1 },
+    { ST_8  , EV_2 , lfsm_always , ST_2 },
+    { ST_8  , EV_3 , lfsm_always , ST_3 },
+    { ST_8  , EV_4 , lfsm_always , ST_4 },
+    { ST_8  , EV_5 , lfsm_always , ST_5 },
+    { ST_8  , EV_6 , lfsm_always , ST_6 },
+    { ST_8  , EV_7 , lfsm_always , ST_7 },
+    { ST_8  , EV_8 , lfsm_always , ST_8 },
+    { ST_8  , EV_9 , lfsm_always , ST_9 },
+    { ST_9  , EV_0 , lfsm_always , ST_0 },
+    { ST_9  , EV_1 , lfsm_always , ST_1 },
+    { ST_9  , EV_2 , lfsm_always , ST_2 },
+    { ST_9  , EV_3 , lfsm_always , ST_3 },
+    { ST_9  , EV_4 , lfsm_always , ST_4 },
+    { ST_9  , EV_5 , lfsm_always , ST_5 },
+    { ST_9  , EV_6 , lfsm_always , ST_6 },
+    { ST_9  , EV_7 , lfsm_always , ST_7 },
+    { ST_9  , EV_8 , lfsm_always , ST_8 },
+    { ST_9  , EV_9 , lfsm_always , ST_9 },
+};
+
 // -------------------------------------------------------------------------
 // - State function table
 // -------------------------------------------------------------------------
@@ -100,6 +207,19 @@ lfsm_state_functions_t state_func_table[] = {
     {ST_NORMAL , normal_entry , normal_run, normal_exit },
     {ST_WARN   , warn_entry   , warn_run  , warn_exit   },
     {ST_ALARM  , alarm_entry  , alarm_run , alarm_exit  },
+};
+
+lfsm_state_functions_t my_state_func_table[] = {
+    { ST_0  , generic_entry , generic_run , generic_exit },
+    { ST_1  , generic_entry , generic_run , generic_exit },
+    { ST_2  , generic_entry , generic_run , generic_exit },
+    { ST_3  , generic_entry , generic_run , generic_exit },
+    { ST_4  , generic_entry , generic_run , generic_exit },
+    { ST_5  , generic_entry , generic_run , generic_exit },
+    { ST_6  , generic_entry , generic_run , generic_exit },
+    { ST_7  , generic_entry , generic_run , generic_exit },
+    { ST_8  , generic_entry , generic_run , generic_exit },
+    { ST_9  , generic_entry , generic_run , generic_exit },
 };
 
 // -- Transition condition functions ----
@@ -132,6 +252,18 @@ lfsm_return_t alarm_run(lfsm_t context){
 }
 lfsm_return_t alarm_exit(lfsm_t context){
     my_data.alarm_exit_run_count++;
+    return LFSM_OK;
+}
+lfsm_return_t generic_entry(lfsm_t context){
+    my_data.generic_entry_run_count++;
+    return LFSM_OK;
+}
+lfsm_return_t generic_run(lfsm_t context){
+    my_data.generic_run_run_count++;
+    return LFSM_OK;
+}
+lfsm_return_t generic_exit(lfsm_t context){
+    my_data.generic_exit_run_count++;
     return LFSM_OK;
 }
 lfsm_return_t warn_entry(lfsm_t context){
@@ -323,4 +455,35 @@ void test_run_non_existing_state_event_combo(void) {
     lfsm_set_state(lfsm_handler, ST_NORMAL);
     fsm_add_event(lfsm_handler, EV_BUTTON_PRESS);
     // lfsm_run(lfsm_handler);
+}
+
+void test_create_large_second_fsm_instance(void) {
+    lfsm_handler = lfsm_init(my_transition_table, my_state_func_table, buffer_callbacks, &my_data, ST_0);
+    TEST_ASSERT_NOT_NULL(lfsm_handler);
+}
+
+void test_execute_all_transitions_once_should_not_crash(void) {
+    int transitions_without_state_change = 0;
+    int transition_count = sizeof(my_transition_table) / sizeof(lfsm_transitions_t);
+
+    test_create_large_second_fsm_instance();
+    for (int state = 0 ; state < 10 ; state++) {
+    for (int event = 0 ; event < 10 ; event++){
+        lfsm_set_state(lfsm_handler, state);
+        fsm_add_event(lfsm_handler, event);
+        lfsm_run(lfsm_handler);
+    }
+    }
+
+    // start at entry 1, cause entry 0 will always go from INVALID to anything
+    lfsm_transitions_t* transition = my_transition_table;
+    transition++;
+    for (int i = 1 ; i < transition_count ; i++) {
+        if (transition->current_state == transition->event) {
+            transitions_without_state_change++;
+        }
+        transition++;
+    }
+    TEST_ASSERT_EQUAL(transition_count - transitions_without_state_change,
+                        my_data.generic_entry_run_count);
 }
